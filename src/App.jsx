4a526ae70c77fea13dcc6e637ecf5fd14c15cb4d
bakhadir <bohodirziyodaliyev@gmail.com>
2023-02-12 18:43:37 +0500

    donr

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import { Routes, Route } from "react-router-dom";
 import Footer from './components/Footer'
 import Navbar from './components/Navbar'
 import Person from './components/Person'
+import Applicant from './pages/Applicant'
+import Companies from './pages/Companies'
+import Empoyer from './pages/Empoyer'
+import Home from './pages/Home'
+import Vacancies from './pages/Vacancies'
+
 
 
 
@@ -14,6 +20,11 @@ const App = () => {
       <Footer />
       <Navbar/>
       <Person/>
+      <Applicant/>
+      <Companies/>
+      <Empoyer/>
+      <Home/>
+      <Vacancies/>
 
 
       <Routes>
